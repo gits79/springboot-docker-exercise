b67f2b3b2b8572dc285278a759eb6f955cf3f9ae
Ji Hwan <g1767g@naver.com>
2023-05-16 14:11:02 +0900

    Feat : commit

---
+++ src/main/java/com/example/springpootdockerexecise/SpringpootDockerExeciseApplication.java
@@ -0,0 +1,13 @@
+package com.example.springpootdockerexecise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringpootDockerExeciseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringpootDockerExeciseApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/springpootdockerexecise/SpringpootDockerExeciseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springpootdockerexecise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringpootDockerExeciseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
